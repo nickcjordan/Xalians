26bdd3e6f7da4ed9687a18430c051c29cd195281
Nick Jordan <njordan1017@gmail.com>
2026-09-10 15:36:11 -0500

    Infra: Terraform-managed Cognito post-confirmation trigger, retire Amplify AdminQueries/PostConfirmation functions
    Fixes #182

--- my-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ my-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -13,26 +13,5 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
-    },
-    "function": {
-        "xalianSignUpSignInResourcePostConfirmation": {
-            "Name": "string",
-            "Arn": "string",
-            "LambdaExecutionRole": "string",
-            "Region": "string"
-        },
-        "AdminQueriesc0a581bf": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    },
-    "api": {
-        "AdminQueries": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        }
     }
 }
